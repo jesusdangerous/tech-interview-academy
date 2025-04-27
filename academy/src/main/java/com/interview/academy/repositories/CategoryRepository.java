8d5cbb07912283288f2ecdda8f0106609ad52005
jesusdangerous <aleksandrzonov641@gmail.com>
2025-04-27 18:41:54 +0500

    Create entity repositories

---
+++ academy/src/main/java/com/interview/academy/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.interview.academy.repositories;
+
+import com.interview.academy.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
---
+++ academy/src/main/java/com/interview/academy/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.interview.academy.repositories;
+
+import com.interview.academy.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+
+}
