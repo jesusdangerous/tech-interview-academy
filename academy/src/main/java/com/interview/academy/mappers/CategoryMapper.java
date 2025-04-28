08e95c4ac5782d417e6062cb4a975d91a6ce0db4
jesusdangerous <aleksandrzonov641@gmail.com>
2025-04-28 14:10:02 +0500

    Create category endpoint, temporary class to simplify working with Insomnia

---
+++ academy/src/main/java/com/interview/academy/mappers/CategoryMapper.java
@@ -0,0 +1,33 @@
+package com.interview.academy.mappers;
+
+import com.interview.academy.domain.PostStatus;
+import com.interview.academy.domain.dtos.CategoryDto;
+import com.interview.academy.domain.dtos.CreateCategoryRequest;
+import com.interview.academy.domain.entities.Category;
+import com.interview.academy.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDto toDto(Category category);
+
+
+    Category toEntity(CreateCategoryRequest createCategoryRequest);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post> posts) {
+        if (posts == null) {
+            return 0;
+        }
+        return posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
