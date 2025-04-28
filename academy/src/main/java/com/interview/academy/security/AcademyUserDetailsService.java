cfdb5f77a6d47f60199c70d2f8c3333924b43fe4
jesusdangerous <aleksandrzonov641@gmail.com>
2025-04-28 17:34:25 +0500

    Add loading users

---
+++ academy/src/main/java/com/interview/academy/security/AcademyUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.interview.academy.security;
+
+import com.interview.academy.domain.entities.User;
+import com.interview.academy.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class AcademyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new AcademyUserDetails(user);
+    }
+}
