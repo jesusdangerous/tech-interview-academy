2873812170d70a99ea91459168964bb880673903
jesusdangerous <aleksandrzonov641@gmail.com>
2025-05-04 18:04:46 +0500

    Add creating tag endpoint

--- academy/src/main/java/com/interview/academy/controllers/TagController.java
+++ academy/src/main/java/com/interview/academy/controllers/TagController.java
@@ -1,14 +1,14 @@
 package com.interview.academy.controllers;
 
+import com.interview.academy.domain.dtos.CreateTagsRequest;
 import com.interview.academy.domain.dtos.TagResponse;
 import com.interview.academy.domain.entities.Tag;
 import com.interview.academy.mappers.TagMapper;
 import com.interview.academy.services.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +26,16 @@ public class TagController {
         List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
 
         return ResponseEntity.ok(tagResponses);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest) {
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+
+        return new ResponseEntity<>(
+                createdTagResponses,
+                HttpStatus.CREATED
+        );
     }
 }
--- academy/src/main/java/com/interview/academy/services/TagService.java
+++ academy/src/main/java/com/interview/academy/services/TagService.java
@@ -3,7 +3,9 @@ package com.interview.academy.services;
 import com.interview.academy.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
