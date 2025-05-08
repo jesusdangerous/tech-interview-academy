7e5e5d0e2d10ca9b168f02ab2d0a5a449cda3edd
jesusdangerous <aleksandrzonov641@gmail.com>
2025-05-08 12:01:53 +0500

    Fix user equals method and add verification field

--- academy/src/main/java/com/interview/academy/domain/entities/User.java
+++ academy/src/main/java/com/interview/academy/domain/entities/User.java
@@ -41,14 +41,29 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Column(name = "verification_code", length = 64)
+    private String verificationCode;
+
+    private boolean enabled;
+
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return enabled == user.enabled &&
+                Objects.equals(id, user.id) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(createdAt, user.createdAt) &&
+                role == user.role &&
+                Objects.equals(verificationCode, user.verificationCode);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, email, password, name, createdAt, role);
+        return Objects.hash(id, email, password, name, createdAt, role, verificationCode, enabled);
     }
 
     @PrePersist
