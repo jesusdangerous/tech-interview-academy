3c9dd9eeb8d4c8241d0184286c3cd17e9b9ad929
jesusdangerous <aleksandrzonov641@gmail.com>
2025-05-10 13:39:32 +0500

    Fix sending emails

--- academy/src/main/java/com/interview/academy/services/impl/EmailServiceImpl.java
+++ academy/src/main/java/com/interview/academy/services/impl/EmailServiceImpl.java
@@ -3,6 +3,7 @@ package com.interview.academy.services.impl;
 import com.interview.academy.domain.entities.Mail;
 import com.interview.academy.services.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -14,14 +15,18 @@ public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     @Override
     @Async
     public void sendVerificationEmail(Mail mail) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("jesusdanerous@yandex.ru");
+        message.setFrom(fromEmail);
         message.setTo(mail.getTo());
         message.setSubject(mail.getSubject());
         message.setText(mail.getBody());
+
 
         mailSender.send(message);
     }
